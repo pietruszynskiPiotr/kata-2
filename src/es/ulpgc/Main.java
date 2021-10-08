50ba007d0eb770c890a107618e851ace162399b8
Piotr Pietruszyński <pietruszynski.piotr@gmail.com>
2021-10-08 22:44:48 +0200

    kata 2 version 1

---
+++ src/es/ulpgc/Main.java
@@ -0,0 +1,24 @@
+package es.ulpgc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] data = new int[] {1, 2, 2,-4, 100, 4, 5, 6, 8, 327, 7, 1, 10, 11, 8, 9, 21, 12, 4, -4};
+        Map<Integer, Integer> histogram = new HashMap<>();
+        for (int number : data) {
+            if (histogram.containsKey(number)) {
+                histogram.put(number, histogram.get(number) + 1);
+            } else {
+                histogram.put(number, 1);
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry : histogram.entrySet()) {
+            System.out.println(entry.getKey() + "==>" + entry.getValue());
+        }
+
+    }
+
+}
