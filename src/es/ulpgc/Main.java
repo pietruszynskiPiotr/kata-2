f387844d4b957d80b8c4360a995f0e2cf1fcd093
Piotr Pietruszyński <pietruszynski.piotr@gmail.com>
2021-10-09 15:48:54 +0200

    kata 2 version 5

--- src/es/ulpgc/Main.java
+++ src/es/ulpgc/Main.java
@@ -5,12 +5,11 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        Integer[] data = new Integer[]{1, 2, 2,-4, 100, 4, 5, 6, 8, 327, 7, 1, 10, 11, 8, 9, 21, 12, 4, -4};
-        Histogram<Integer> histogram = new Histogram(data);
-        for (Map.Entry<Integer, Integer> entry : histogram.getHistogram().entrySet()) {
+        String[] names = new String[]{"John", "Piotr", "Pedro", "Michael", "Lucy", "Sofia", "John", "Stephen", "Michael", "Harry", "Anne", "Michael"};
+        Histogram<String> histogram = new Histogram(names);
+        for (Map.Entry<String, Integer> entry : histogram.getHistogram().entrySet()) {
             System.out.println(entry.getKey() + "==>" + entry.getValue());
         }
-
     }
 
 }
