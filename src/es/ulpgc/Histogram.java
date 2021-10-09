6408c2e0e8cd4d7e0b594eff2d23d9d8fd07aedf
Piotr Pietruszyński <pietruszynski.piotr@gmail.com>
2021-10-09 14:33:03 +0200

    kata 2 version 4

--- src/es/ulpgc/Histogram.java
+++ src/es/ulpgc/Histogram.java
@@ -3,17 +3,21 @@ package es.ulpgc;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Histogram {
+public class Histogram<T> {
 
-    private final int[] numbers;
+    private final T[] numbers;
 
-    public Histogram(int[] numbers) {
+    public Histogram(T[] numbers) {
         this.numbers = numbers;
     }
 
-    public Map<Integer, Integer> getHistogram() {
-        HashMap<Integer, Integer> histogram = new HashMap<>();
-        for (int number : numbers) {
+    public T[] numbers() {
+        return numbers;
+    }
+
+    public Map<T, Integer> getHistogram() {
+        HashMap<T, Integer> histogram = new HashMap<>();
+        for (T number : numbers) {
             histogram.put(number, histogram.containsKey(number) ? histogram.get(number) + 1 : 1);
         }
         return histogram;
--- src/es/ulpgc/Main.java
+++ src/es/ulpgc/Main.java
@@ -5,8 +5,8 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        int[] data = new int[] {1, 2, 2,-4, 100, 4, 5, 6, 8, 327, 7, 1, 10, 11, 8, 9, 21, 12, 4, -4};
-        Histogram histogram = new Histogram(data);
+        Integer[] data = new Integer[]{1, 2, 2,-4, 100, 4, 5, 6, 8, 327, 7, 1, 10, 11, 8, 9, 21, 12, 4, -4};
+        Histogram<Integer> histogram = new Histogram(data);
         for (Map.Entry<Integer, Integer> entry : histogram.getHistogram().entrySet()) {
             System.out.println(entry.getKey() + "==>" + entry.getValue());
         }
